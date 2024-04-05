e3a75e578c3d990cda657be7ca0fa89337496a40
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-04-05 09:27:17 -0400

    clean-up config and env

--- config.py
+++ config.py
@@ -9,18 +9,11 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     FLASK_ENV = 'development'
-    # Add development-specific configurations, like database URIs
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
-    # Add testing-specific configurations
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    # Production configs like database URIs
-    # Logging configurations
-    # Security configurations, like SSL
-
-# You can add more configurations for different environments as needed
